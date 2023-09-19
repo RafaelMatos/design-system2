6ee6292d0635316a29c61d6865f223993307c1db
RafaelMatos <rafael-matos@outlook.com>
2023-09-19 15:43:59 -0300

    initial commit -stitches configurado

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,13 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  height: '$10',
+  width: '$10',
+})
+
+export function App() {
+  return <Button>Hello World</Button>
+}
