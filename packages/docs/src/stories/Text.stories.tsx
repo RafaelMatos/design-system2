9da3bea9e6a1526491f6ab7e18dda19d1b1ba57d
RafaelMatos <rafael-matos@outlook.com>
2023-09-22 00:26:28 -0300

    stories - created docs for Box and Text Components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react/src/components/Text'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos, labore consectetur. Possimus labore, ab necessitatibus, obcaecati quidem dolorem soluta exercitationem, debitis magnam tenetur ea? Libero distinctio architecto laudantium nemo quis.',
+  },
+} as Meta<TextProps>
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
