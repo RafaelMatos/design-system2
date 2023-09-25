00b71a5f7c0f5d3e6103f0042a579e864015df62
RafaelMatos <rafael-matos@outlook.com>
2023-09-25 17:03:26 -0300

    stories - created doc for Avatar Component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'http://github.com/RafaelMatos.png',
+    alt: 'Rafael Matos',
+  },
+} as Meta<AvatarProps>
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
