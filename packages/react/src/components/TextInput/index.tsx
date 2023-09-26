494da3f5342e3d9309e7143384f4cf7df18c74fe
RafaelMatos <rafael-matos@outlook.com>
2023-09-26 17:46:50 -0300

    stories - created doc for TextInput Component

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps, ElementType } from 'react'
+import { Input, Prefixe, TextInputContainer } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefixe>{prefix}</Prefixe>}
+      <Input {...props} />
+    </TextInputContainer>
+  )
+}
